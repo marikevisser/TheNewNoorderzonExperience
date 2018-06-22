63070005610d007b1cf19bd52f147c3f31e47516
TSluizeman <t.sluizeman@gmail.com>
2018-06-22 11:02:01 +0200

    database mappen structuur aangepast & invoer formulier aangepast

---
+++ src/main/application/ArtiestRepository.java
@@ -0,0 +1,19 @@
+package main.application;
+
+import nl.hanze.application.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Repository
+public interface ProgramRepository extends JpaRepository<!-User-!, Integer> {
+	User findByProgramNameEquals(String programname);
+
+	User deleteById(int id);
+
+}
